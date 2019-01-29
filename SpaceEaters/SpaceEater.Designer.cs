d48220cbf61bb647cc4d7774e49ba324a93d78ae
Rahul <ukrhul@gmail.com>
2019-01-28 22:12:37 -0500

    first commit

---
+++ SpaceEaters/SpaceEater.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SpaceEaters
+{
+    partial class frmrectangularCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblOutputPerimeter = new System.Windows.Forms.Label();
+            this.lblOutputArea = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblRectangle = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.RectangleCalculator = new System.Windows.Forms.ToolTip(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutputPerimeter
+            // 
+            this.lblOutputPerimeter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutputPerimeter.Location = new System.Drawing.Point(153, 181);
+            this.lblOutputPerimeter.Name = "lblOutputPerimeter";
+            this.lblOutputPerimeter.Size = new System.Drawing.Size(87, 20);
+            this.lblOutputPerimeter.TabIndex = 9;
+            // 
+            // lblOutputArea
+            // 
+            this.lblOutputArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutputArea.Location = new System.Drawing.Point(34, 181);
+            this.lblOutputArea.Name = "lblOutputArea";
+            this.lblOutputArea.Size = new System.Drawing.Size(75, 20);
+            this.lblOutputArea.TabIndex = 8;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(104, 219);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Re&set";
+            this.RectangleCalculator.SetToolTip(this.btnReset, "Click to reset the form");
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(92, 137);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(52, 13);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Text = "OUTPUT";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRectangle
+            // 
+            this.lblRectangle.AutoSize = true;
+            this.lblRectangle.Location = new System.Drawing.Point(92, 19);
+            this.lblRectangle.Name = "lblRectangle";
+            this.lblRectangle.Size = new System.Drawing.Size(56, 13);
+            this.lblRectangle.TabIndex = 0;
+            this.lblRectangle.Text = "Rectangle";
+            this.lblRectangle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(31, 154);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(29, 13);
+            this.lblArea.TabIndex = 6;
+            this.lblArea.Text = "Area";
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Location = new System.Drawing.Point(150, 154);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(51, 13);
+            this.lblPerimeter.TabIndex = 7;
+            this.lblPerimeter.Text = "Perimeter";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 219);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "&Calculate";
+            this.RectangleCalculator.SetToolTip(this.btnCalculate, "Click to calculate the Area and Perimeter of Rectangle");
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(153, 102);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 4;
+            this.RectangleCalculator.SetToolTip(this.txtWidth, "Enter Width of the rectangle");
+            this.txtWidth.TextChanged += new System.EventHandler(this.txtWidth_TextChanged);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(153, 49);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 2;
+            this.RectangleCalculator.SetToolTip(this.txtLength, "Enter Length of the rectangle ");
+            this.txtLength.TextChanged += new System.EventHandler(this.txtLength_TextChanged);
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(42, 102);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 3;
+            this.lblWidth.Text = "Wi&dth";
+            // 
+            // lblLength
+            // 
+            this.lblLength.Location = new System.Drawing.Point(42, 52);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 1;
+            this.lblLength.Text = "&Length:";
+            this.lblLength.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 219);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "&Exit";
+            this.RectangleCalculator.SetToolTip(this.btnExit, "Cilck to exit the form");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmrectangularCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblOutputPerimeter);
+            this.Controls.Add(this.lblOutputArea);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblRectangle);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Name = "frmrectangularCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Space Eaters";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutputPerimeter;
+        private System.Windows.Forms.Label lblOutputArea;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ToolTip RectangleCalculator;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblRectangle;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SpaceEaters/SpaceEater.cs
@@ -0,0 +1,107 @@
+﻿/* Program Name: ICE-14 "Space Eaters"
+ * Programmed By: Rahul Upadhyay
+ * Last Updated: April 05, 2017
+ * 
+ * Description: It is the windows form apllication to calculate the area and perimeter of the rectangle.
+ *              User input the length of the rectangle in txtLength field and the width in txtWidth field.
+ *              If user clicks the calcualte button then it will calculate the area and perimeter and display it in lblOutputArea and lblOutputPerimeter.
+ *              If user clicks on Exit button it will close the form and if user clicks on Reset button it will reset the form. 
+ * */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceEaters
+{
+    public partial class frmrectangularCalculator : Form
+    {
+        public frmrectangularCalculator()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is for when user changed the width text value
+        /// </summary>
+        private void txtLength_TextChanged(object sender, EventArgs e)
+        {
+            lblOutputPerimeter.Text = "";   //set Perimeter Output to blank
+            lblOutputArea.Text = "";        //set Area Output to blank
+            txtLength.Focus();              //set focus to Length field
+        }
+
+        /// <summary>
+        /// it is for calculate button and error messages. 
+        /// </summary>
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declarations:
+
+            decimal rectangleLength;
+            decimal rectangleWidth;
+            decimal rectangleArea;
+            decimal rectanglePerimeter;
+
+            if (Decimal.TryParse(txtLength.Text, out rectangleLength) && (rectangleLength > 0) == false) //check user input in length field is numeric or not and > than 0
+            {
+                MessageBox.Show("Please enter a numeric value.");     
+                txtLength.Focus();      //set focus to length
+                
+            }
+
+            if (Decimal.TryParse(txtWidth.Text, out rectangleWidth) && (rectangleWidth > 0) == false)   //check user input in Width field is numeric or not and > than 0
+            {
+                MessageBox.Show("Please enter a numeric value.");
+                txtWidth.Focus();      //set focus to width
+            }
+
+            else
+            {
+                //Processing
+                rectangleArea = rectangleLength * rectangleWidth;            //Area of rectangle
+                rectanglePerimeter = 2*(rectangleLength + rectangleWidth);   //Perimeter of rectangle
+
+                //OUTPUT
+                lblOutputArea.Text = Math.Round(rectangleArea,2).ToString();             //Output the Area in lblOuputArea
+                lblOutputPerimeter.Text=Math.Round(rectanglePerimeter,2).ToString();     //Output the Perimeter in lblOutputPerimeter
+            }
+        }
+
+        /// <summary>
+        ///this is for reset button 
+        /// </summary>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtLength.Text = "";
+            txtWidth.Text  = "";
+            lblOutputArea.Text = "";
+            lblOutputPerimeter.Text = "";
+            txtLength.Focus();
+
+        }
+
+        /// <summary>
+        /// this closes the form
+        /// </summary>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();      //closes the form
+        }
+
+        /// <summary>
+        /// This is for when user changed the width text value
+        /// </summary>
+        private void txtWidth_TextChanged(object sender, EventArgs e)
+        {
+            lblOutputArea.Text = "";            //set Area Output to blank
+            lblOutputPerimeter.Text = "";       //set Perimeter Output to blank
+            txtWidth.Focus();                   //set focus to Width field
+        }
+    }
+}
